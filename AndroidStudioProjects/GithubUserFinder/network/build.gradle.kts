6884335b80697f124e752b1a07109854862b88bb
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2025-02-20 20:23:24 -0400

    add plugins Project

--- AndroidStudioProjects/GithubUserFinder/network/build.gradle.kts
+++ AndroidStudioProjects/GithubUserFinder/network/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ucb.android.library)
     alias(libs.plugins.kotlin.kapt)
-
 }
 
 android {
